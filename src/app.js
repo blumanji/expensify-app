4a9346e5817b1df15990b49d5514b6d91e9eca4f
blumanji <blumanji@gmail.com>
2018-10-01 23:21:53 -0400

    Installed React Router and some changes
    Installed React Router and started working through api examples and building a page

--- src/app.js
+++ src/app.js
@@ -3,7 +3,68 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my EditExpensePage
+    </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my Help Page
+    </div>
+
+);
+
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go Home</Link>
+    </div>
+
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Home</Link>
+    <Link to="create">Create</Link>
+    <Link to="edit">Edit</Link>
+    <Link to="help">Help</Link>
+    </header>
+);
+
+
+const routes = (
+  <BrowserRouter>
+  <div>
+    <Header /> 
+    <Switch>
+    <Route path="/" component={ExpenseDashboardPage} exact={true} />
+    <Route path="/create" component={AddExpensePage} />
+    <Route path="/edit" component={EditExpensePage} />
+    <Route path="/help" component={HelpPage} />
+    <Route component={NotFoundPage} />
+  </Switch>
+  </div>
+ 
+  </BrowserRouter>
+  
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
